5101ae382d3165be11829d63958515648390bd96
asia_demartino@libero.it <asia_demartino@libero.it>
2022-03-22 12:45:21 +0100

    aggiunti pacchetti necessari + inizio header

---
+++ src/components/MyInfo.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="info d-flex justify-content-between text-white">
+    <div class="info-sx d-flex align-items-center gap-2 ">
+      <i class="fa-solid fa-clock"></i>
+      <span>Open Hours: Mon - Sat - 9:00 - 18:00</span>
+    </div>
+    <div class="info-dx d-flex align-items-center gap-2 ">
+      <i class="fa-solid fa-phone"></i>
+      <span>+1 (305) 1234-5678</span>
+      <i class="fa-solid fa-envelope"></i>
+      <span>hello@example.com</span>
+      
+    
+   </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyInfo',
+}
+</script>
+
+<style lang="scss" scoped>
+@import '/src/assets/mixin';
+
+.info{
+  height: 33px;
+  background-color: rgb(26,38,47);
+  font-size: 12px;
+  @include padding-laterale;
+
+  .info-sx{
+  height: 33px;
+  }
+
+  .info-dx i{ 
+    margin-left: 12px;
+  }
+}
+
+</style>
